0b5762288a226d632e2b2bced0a04ab334a7c407
Alexandre Gaillet <alexandre.gaillet@fr.clara.net>
2020-02-21 18:13:56 +0100

    AZ-196 Support hubs creation

--- locals.tf
+++ locals.tf
@@ -7,38 +7,38 @@ locals {
   # Generate a default name
   default_name = "${var.stack}-${var.client_name}-${var.location_short}-${var.environment}"
 
-  # Generate a list of queues to create
-  queues_list = flatten(
-    [for namespace, values in var.servicebus_namespaces_queues :
-      [for queuename in keys(lookup(values, "queues", {})) :
-        "${namespace}|${queuename}"
+  # Generate a list of hubs to create
+  hubs_list = flatten(
+    [for namespace, values in var.eventhub_namespaces_hubs :
+      [for hubname in keys(lookup(values, "hubs", {})) :
+        "${namespace}|${hubname}"
       ]
     ]
   )
 
-  # Generate a list of queues to create shared access policies with reader right
-  queues_reader = flatten(
-    [for namespace, values in var.servicebus_namespaces_queues :
-      [for queuename, params in lookup(values, "queues", {}) :
-        "${namespace}|${queuename}" if lookup(params, "reader", false)
+  # Generate a list of hubs to create shared access policies with reader right
+  hubs_reader = flatten(
+    [for namespace, values in var.eventhub_namespaces_hubs :
+      [for hubname, params in lookup(values, "hubs", {}) :
+        "${namespace}|${hubname}" if lookup(params, "reader", false)
       ]
     ]
   )
 
-  # Generate a list of queues to create shared access policies with sender right
-  queues_sender = flatten(
-    [for namespace, values in var.servicebus_namespaces_queues :
-      [for queuename, params in lookup(values, "queues", {}) :
-        "${namespace}|${queuename}" if lookup(params, "sender", false)
+  # Generate a list of hubs to create shared access policies with sender right
+  hubs_sender = flatten(
+    [for namespace, values in var.eventhub_namespaces_hubs :
+      [for hubname, params in lookup(values, "hubs", {}) :
+        "${namespace}|${hubname}" if lookup(params, "sender", false)
       ]
     ]
   )
 
-  # Generate a list of queues to create shared access policies with manage right
-  queues_manage = flatten(
-    [for namespace, values in var.servicebus_namespaces_queues :
-      [for queuename, params in lookup(values, "queues", {}) :
-        "${namespace}|${queuename}" if lookup(params, "manage", false)
+  # Generate a list of hubs to create shared access policies with manage right
+  hubs_manage = flatten(
+    [for namespace, values in var.eventhub_namespaces_hubs :
+      [for hubname, params in lookup(values, "hubs", {}) :
+        "${namespace}|${hubname}" if lookup(params, "manage", false)
       ]
     ]
   )
